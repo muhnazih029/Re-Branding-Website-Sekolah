fe6fdde08cc927faafe90082f59e0919726a1983
Muhammad Nazih <muhammadnazih029@gmail.com>
2025-03-20 18:50:30 +0700

    refactor(setting): 🦄 model
    make an table name and fillable in model

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Setting extends Model
 {
-    //
+    protected $table = 'settings';
+    protected $fillable = [
+        'key',
+        'label',
+        'value',
+        'type',
+        'image',
+    ];
 }
