85dd85e357df509b6d22b811e237093e210caa3a
putrijuliaa <putrijulia866@gmail.com>
2025-03-24 15:31:37 +0700

    sidebarrr

--- resources/views/pages/profile/teachers.blade.php
+++ resources/views/pages/profile/teachers.blade.php
@@ -471,21 +471,21 @@
                     class="w-[250px] h-[45vh] shadow-md p-5 mb-5 md:mb-0">
                     <h2 class="text-xl font-bold mb-4">Profil</h2>
                     <ul class="space-y-2">
-                        <li><a href="#" class="block text-[#7886C7] hover:text-blue-900 font-semibold">Visi Misi
+                        <li><a href="/profile/visi-misi" class="block text-[#7886C7] hover:text-blue-900 font-semibold">Visi Misi
                                 Sekolah</a></li>
-                        <li><a href="#" class="block text-[#7886C7] hover:text-blue-900 font-semibold">NPSN</a>
+                        <li><a href="https://referensi.data.kemdikbud.go.id/tabs.php?npsn=20329336" class="block text-[#7886C7] hover:text-blue-900 font-semibold">NPSN</a>
                         </li>
-                        <li><a href="#" class="block text-[#7886C7] hover:text-blue-900 font-semibold">Sejarah
+                        <li><a href="/profile/sejarah-sekolah" class="block text-[#7886C7] hover:text-blue-900 font-semibold">Sejarah
                                 Sekolah</a></li>
-                        <li><a href="#" class="block text-[#7886C7] hover:text-blue-900 font-semibold">Pendidik
+                        <li><a href="/profile/pendidik" class="block text-[#7886C7] hover:text-blue-900 font-semibold">Pendidik
                                 dan Tenaga Kependidikan</a></li>
-                        <li><a href="#" class="block text-[#7886C7] hover:text-blue-900 font-semibold">Peserta
+                        <li><a href="/profile/peserta-didik" class="block text-[#7886C7] hover:text-blue-900 font-semibold">Peserta
                                 Didik</a></li>
-                        <li><a href="#"
+                        <li><a href="/profile/ekstrakulikuler"
                                 class="block text-[#7886C7] hover:text-blue-900 font-semibold">Ekstrakurikuler</a></li>
-                        <li><a href="#" class="block text-[#7886C7] hover:text-blue-900 font-semibold">BOS
+                        <li><a href="/profile/dana-bos" class="block text-[#7886C7] hover:text-blue-900 font-semibold">BOS
                                 Bantuan Operasional Sekolah</a></li>
-                        <li><a href="#" class="block text-[#7886C7] hover:text-blue-900 font-semibold">Sarana
+                        <li><a href="/profile/sarana-prasarana" class="block text-[#7886C7] hover:text-blue-900 font-semibold">Sarana
                                 dan Prasarana</a></li>
                     </ul>
                 </div>
