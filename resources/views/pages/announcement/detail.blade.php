29a56e951647571a23877da38f7c29c013a57fac
Muhammad Nazih <muhammadnazih029@gmail.com>
2025-03-26 13:48:08 +0700

    refactor(detail): 🦄 component
    add header, navbar and footer

--- resources/views/pages/announcement/detail.blade.php
+++ resources/views/pages/announcement/detail.blade.php
@@ -1,4 +1,6 @@
-{{ 'ini halaman detail pengumuman' }}
+<x-layouts>
+    <x-layouts.header />
+
 <div class="max-w-3xl p-6 mx-auto mt-10 bg-white rounded-lg shadow-md">
     <h1 class="text-2xl font-bold text-blue-900">{{ $announcement->title }}</h1>
 
@@ -7,7 +9,7 @@
         {{ \Carbon\Carbon::parse($announcement->created_at)->format('d M Y') }}
     </p>
 
-    <img src="{{ Storage::url($announcement->image ?? 'default.jpg') }}" alt="{{ $announcement->title }}"
+    <img src="{{ Storage::url($announcement->image) }}" alt="{{ $announcement->title }}"
         class="w-full h-auto mt-4 rounded-lg" height="100px">
 
     <div class="mt-6 text-gray-700">
@@ -19,3 +21,5 @@
         ← Kembali
     </a>
 </div>
+<x-layouts.footer />
+</x-layouts>
