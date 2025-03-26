b4b94eb5adbd6be9a215107f16b26816a3e74173
Muhammad Nazih <muhammadnazih029@gmail.com>
2025-03-26 13:49:04 +0700

    add route

--- resources/views/pages/announcement/detail.blade.php
+++ resources/views/pages/announcement/detail.blade.php
@@ -1,25 +1,25 @@
 <x-layouts>
     <x-layouts.header />
 
-<div class="max-w-3xl p-6 mx-auto mt-10 bg-white rounded-lg shadow-md">
-    <h1 class="text-2xl font-bold text-blue-900">{{ $announcement->title }}</h1>
+    <div class="max-w-3xl p-6 mx-auto mt-10 bg-white rounded-lg shadow-md">
+        <h1 class="text-2xl font-bold text-blue-900">{{ $announcement->title }}</h1>
 
-    <p class="mt-2 text-sm text-gray-500">
-        Ditulis oleh: {{ $announcement->author }} |
-        {{ \Carbon\Carbon::parse($announcement->created_at)->format('d M Y') }}
-    </p>
+        <p class="mt-2 text-sm text-gray-500">
+            Ditulis oleh: {{ $announcement->author }} |
+            {{ \Carbon\Carbon::parse($announcement->created_at)->format('d M Y') }}
+        </p>
 
-    <img src="{{ Storage::url($announcement->image) }}" alt="{{ $announcement->title }}"
-        class="w-full h-auto mt-4 rounded-lg" height="100px">
+        <img src="{{ Storage::url($announcement->image) }}" alt="{{ $announcement->title }}"
+            class="w-full h-auto mt-4 rounded-lg" height="100px">
 
-    <div class="mt-6 text-gray-700">
-        {!! nl2br(e($announcement->content)) !!}
+        <div class="mt-6 text-gray-700">
+            {!! nl2br(e($announcement->content)) !!}
+        </div>
+
+        <a href="{{ route('announcement.index') }}"
+            class="inline-block px-4 py-2 mt-6 text-white bg-blue-900 rounded-lg hover:bg-blue-800">
+            ← Kembali
+        </a>
     </div>
-
-    <a href="{{ url()->previous() }}"
-        class="inline-block px-4 py-2 mt-6 text-white bg-blue-900 rounded-lg hover:bg-blue-800">
-        ← Kembali
-    </a>
-</div>
-<x-layouts.footer />
+    <x-layouts.footer />
 </x-layouts>
