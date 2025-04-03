48acbec47eae73e07d1848d4594aaa200b735097
Muhammad Nazih <muhammadnazih029@gmail.com>
2025-04-03 23:20:53 +0700

    init(Dana bos): 🎉 controller & view
    make an controller and store data to view bos

--- app/Http/Controllers/SchoolOperationalAssistanceController.php
+++ app/Http/Controllers/SchoolOperationalAssistanceController.php
@@ -2,18 +2,36 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\SchoolOperationalAssistance;
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use App\Models\SchoolOperationalAssistance;
 
 class SchoolOperationalAssistanceController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('pages.profile.bos');
+        $currentYear = date('Y');
+        $years = range($currentYear, $currentYear - 5);
+        $year = $request->input('year', $currentYear);
+
+        $assistances = DB::table('school_operational_assistances')
+            ->whereYear('date', $year)
+            ->select(
+                DB::raw("strftime('%m', date) as month"),
+                DB::raw("SUM(receipt) as total_receipt"),
+                DB::raw("SUM(expenditure) as total_expenditure"),
+                DB::raw("SUM(bank_balance) as total_balance")
+            )
+            ->whereRaw("strftime('%Y', date) = ?", [$year]) // Filter berdasarkan tahun
+            ->groupBy(DB::raw("strftime('%m', date)"))
+            ->orderBy(DB::raw("strftime('%m', date)"))
+            ->get();
+
+        return view('pages.profile.bos', compact('assistances', 'year', 'years'));
     }
 
     /**
