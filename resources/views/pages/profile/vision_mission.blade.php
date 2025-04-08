5f0c600b385804abeed07847345918ef06866052
Muhammad Nazih <muhammadnazih029@gmail.com>
2025-04-08 16:44:08 +0700

    refactor(visi misi): 🦄 blade view
    implement component aside for profile

--- resources/views/pages/profile/vision_mission.blade.php
+++ resources/views/pages/profile/vision_mission.blade.php
@@ -1,18 +1,18 @@
 <x-layouts>
     <x-layouts.header />
     <x-profiles.hero />
-   <!-- Konten Utama -->
-   <div class="flex items-center justify-center min-h-screen mt-4 bg-gray-100">
-    <div class="container p-5 pl-10 mx-auto md:pl-20">
-        <!-- Kotak PROFIL dan Sejarah Sekolah -->
-        <div class="flex flex-col mb-5 space-y-5 md:flex-row md:items-start md:space-x-5 md:space-y-0">
-            <div style="background-color: #2D336B;" class="w-[200px] shadow-md p-5 text-center">
-                <h1 class="text-xl font-bold text-white">PROFIL</h1>
+    <!-- Konten Utama -->
+    <div class="flex items-center justify-center min-h-screen mt-4 bg-gray-100">
+        <div class="container p-5 pl-10 mx-auto md:pl-20">
+            <!-- Kotak PROFIL dan Sejarah Sekolah -->
+            <div class="flex flex-col mb-5 space-y-5 md:flex-row md:items-start md:space-x-5 md:space-y-0">
+                <div style="background-color: #2D336B;" class="w-[200px] shadow-md p-5 text-center">
+                    <h1 class="text-xl font-bold text-white">PROFIL</h1>
+                </div>
+                <div style="background-color: #2D336B;" class="w-[300px] shadow-md p-5 text-center md:ml-5 md:mt-0 mt-0">
+                    <h1 class="text-xl font-bold text-white">Visi Misi</h1>
+                </div>
             </div>
-            <div style="background-color: #2D336B;" class="w-[300px] shadow-md p-5 text-center md:ml-5 md:mt-0 mt-0">
-                <h1 class="text-xl font-bold text-white">Visi Misi</h1>
-            </div>
-        </div>
 
             <!-- Garis Pembatas -->
             <div class="w-[200px] border-b-4 border-blue-900 mb-5"></div>
@@ -20,36 +20,7 @@
             <!-- Konten dan Sidebar -->
             <div class="flex flex-col md:flex-row">
                 <!-- Sidebar Menu Profil -->
-                <div style="background-color: #E6EAF7; color:#2D336B;"
-                    class="w-[250px] h-[45vh] shadow-md p-5 mb-5 md:mb-0 flex-shrink-0 flex-grow-0">
-                    <h2 class="mb-4 text-xl font-bold">Profil</h2>
-                    <ul class="space-y-2">
-                        <li><a href="/profile/visi-misi"
-                                class="block text-[#7886C7] hover:text-blue-900 font-semibold">Visi Misi
-                                Sekolah</a></li>
-                        <li><a href="https://referensi.data.kemdikbud.go.id/tabs.php?npsn=20329336"
-                                class="block text-[#7886C7] hover:text-blue-900 font-semibold">NPSN</a>
-                        </li>
-                        <li><a href="/profile/sejarah-sekolah"
-                                class="block text-[#7886C7] hover:text-blue-900 font-semibold">Sejarah
-                                Sekolah</a></li>
-                        <li><a href="/profile/pendidik"
-                                class="block text-[#7886C7] hover:text-blue-900 font-semibold">Pendidik
-                                dan Tenaga Kependidikan</a></li>
-                        <li><a href="/profile/peserta-didik"
-                                class="block text-[#7886C7] hover:text-blue-900 font-semibold">Peserta
-                                Didik</a></li>
-                        <li><a href="/profile/ekstrakulikuler"
-                                class="block text-[#7886C7] hover:text-blue-900 font-semibold">Ekstrakurikuler</a>
-                        </li>
-                        <li><a href="/profile/dana-bos"
-                                class="block text-[#7886C7] hover:text-blue-900 font-semibold">BOS
-                                Bantuan Operasional Sekolah</a></li>
-                        <li><a href="/profile/sarana-prasarana"
-                                class="block text-[#7886C7] hover:text-blue-900 font-semibold">Sarana
-                                dan Prasarana</a></li>
-                    </ul>
-                </div>
+                <x-profiles.aside />
 
                 <!-- Konten -->
                 <div class="w-full h-full p-5 bg-gray-100 rounded-lg md:w-3/4 md:ml-5">
