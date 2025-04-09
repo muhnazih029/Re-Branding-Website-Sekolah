a12185c9ef3227cffcfffe1eb5636a7a885428bd
aliifakmal <aliifakmal1009@gmail.com>
2025-04-09 08:41:24 +0700

    add gambar ppdb fake

--- resources/views/pages/announcement/new_student_registration.blade.php
+++ resources/views/pages/announcement/new_student_registration.blade.php
@@ -14,21 +14,24 @@
         <!-- Tittle -->
         <div class="mt-4 text-center">
             <h1 class="text-lg font-bold text-blue-900 md:text-xl">
-                Berikut adalah siswa yang dinyatakan diterima di SDN Bandarharjo 01
+               Pendaftaran Peserta Didik Baru (PPDB)
             </h1>
             <h2 class="text-lg font-bold text-blue-900 md:text-xl">
-                Kota Semarang Tahun Ajaran 2024/2025
+                SDN Bandarharjo 01 Kota Semarang
+            </h2>
+            <h2 class="text-lg font-bold text-blue-900 md:text-xl">
+                Tahun Ajaran 2024/2025
             </h2>
         </div>
 
         <!-- Gambar Dokumen -->
         <div class="flex justify-center mt-4">
             <img src="/img/ppdb.png" alt="Pendaftaran PPDB"
-                class="w-full h-auto max-w-2xl rounded-lg shadow-lg sm:w-4/5 md:w-3/4 lg:w-1/2 xl:w-1/3">
+                class="w-full h-auto max-w-2xl rounded-lg shadow-lg">
         </div>
     </div>
 
-    <!-- Breadcrumb Navigation -->
+    <!-- Breadcrumb -->
     <div class="flex items-center mb-10 ml-8 space-x-2">
         <x-breadcrumb />
     </div>
