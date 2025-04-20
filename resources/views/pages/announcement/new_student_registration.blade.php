a855476e2a00356b39f2d285808baba48c74a5ce
Muhammad Nazih <muhammadnazih029@gmail.com>
2025-04-20 22:28:07 +0700

    style(ppdb): 🌈 pagination
    change the style of breadcrumb with ml tailwind

--- resources/views/pages/announcement/new_student_registration.blade.php
+++ resources/views/pages/announcement/new_student_registration.blade.php
@@ -14,7 +14,7 @@
         <!-- Tittle -->
         <div class="mt-4 text-center">
             <h1 class="text-lg font-bold text-blue-900 md:text-xl">
-               Penerimaan Peserta Didik Baru (PPDB) SDN Bandarharjo 01 Kota Semarang
+                Penerimaan Peserta Didik Baru (PPDB) SDN Bandarharjo 01 Kota Semarang
             </h1>
             <h2 class="text-lg font-bold text-blue-900 md:text-xl">
                 Tahun Ajaran 2025/2026
@@ -23,13 +23,13 @@
 
         <!-- Gambar Dokumen -->
         <div class="flex justify-center mt-12">
-            <img src="/img/ppdb.jpeg" alt="Pendaftaran PPDB"
-                class="w-full h-auto max-w-2xl rounded-lg shadow-xl">
+            <img src="/img/ppdb.jpeg" alt="Pendaftaran PPDB" class="w-full h-auto max-w-2xl rounded-lg shadow-xl">
         </div>
 
         {{-- Link Pendaftaran --}}
         <div class="mt-10 text-center">
-            <h3>Pendaftaran Peserta Didik Baru SDN Bandarharjo 01 Kota Semarang Tahun Ajaran 2025/2026 dapat dilakukan dengan klik <b>'Daftar'</b> di bawah ini</h3>
+            <h3>Pendaftaran Peserta Didik Baru SDN Bandarharjo 01 Kota Semarang Tahun Ajaran 2025/2026 dapat dilakukan
+                dengan klik <b>'Daftar'</b> di bawah ini</h3>
             <a href="https://ppd.semarangkota.go.id/sd" target="_blank" rel="noopener noreferrer"
                 class="inline-block bg-[#2D336B] hover:bg-[#1e254f] text-white font-semibold py-3 px-6 mt-2 rounded-lg shadow-md transition duration-300">
                 Daftar
@@ -39,7 +39,7 @@
     </div>
 
     <!-- Breadcrumb -->
-    <div class="flex items-center mb-10 ml-8 space-x-2">
+    <div class="flex items-center mb-10 ml-24 space-x-2">
         <x-breadcrumb />
     </div>
     <x-layouts.footer />
