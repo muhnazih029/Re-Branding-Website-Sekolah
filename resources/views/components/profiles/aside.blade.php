955f1c9afbe4103857c95c874ae29229793368f9
Muhammad Nazih <muhammadnazih029@gmail.com>
2025-04-20 22:29:09 +0700

    style(profile): 🌈 aside
    make the aside page aktive to have different color

--- resources/views/components/profiles/aside.blade.php
+++ resources/views/components/profiles/aside.blade.php
@@ -1,22 +1,54 @@
-<div style="background-color: #E6EAF7; color:#2D336B;" class="w-[250px] h-[fit-content] max-h-[80vh] overflow-y-auto shadow-md p-5 mb-5 md:mb-0">
+<div style="background-color: #E6EAF7;"
+    class="w-[250px] h-[fit-content] max-h-[80vh] overflow-y-auto shadow-md p-5 mb-5 md:mb-0">
     <h2 class="mb-4 text-xl font-bold">Profil</h2>
     <ul class="space-y-2">
-        <li><a href="/profile/visi-misi" class="block text-[#7886C7] hover:text-blue-900 font-semibold">Visi Misi
-                Sekolah</a></li>
-        <li><a href="https://referensi.data.kemdikbud.go.id/tabs.php?npsn=20329336"
-                class="block text-[#7886C7] hover:text-blue-900 font-semibold">NPSN</a>
+        <li>
+            <a href="{{ route('profile.vision_mission') }}"
+                class="block {{ request()->routeIs('profile.vision_mission') ? 'text-[#2D336B]' : 'text-[#7886C7]' }} hover:text-blue-900 font-semibold">
+                Visi Misi Sekolah
+            </a>
         </li>
-        <li><a href="/profile/sejarah-sekolah" class="block text-[#7886C7] hover:text-blue-900 font-semibold">Sejarah
-                Sekolah</a></li>
-        <li><a href="/profile/pendidik" class="block text-[#7886C7] hover:text-blue-900 font-semibold">Pendidik
-                dan Tenaga Kependidikan</a></li>
-        <li><a href="/profile/peserta-didik" class="block text-[#7886C7] hover:text-blue-900 font-semibold">Peserta
-                Didik</a></li>
-        <li><a href="/profile/ekstrakulikuler"
-                class="block text-[#7886C7] hover:text-blue-900 font-semibold">Ekstrakurikuler</a></li>
-        <li><a href="/profile/dana-bos" class="block text-[#7886C7] hover:text-blue-900 font-semibold">Bantuan
-                Operasional Sekolah</a></li>
-        <li><a href="/profile/sarana-prasarana" class="block text-[#7886C7] hover:text-blue-900 font-semibold">Sarana
-                dan Prasarana</a></li>
+        <li>
+            <a href="https://referensi.data.kemdikbud.go.id/tabs.php?npsn=20329336" target="_blank"
+                class="block text-[#7886C7] hover:text-blue-900 font-semibold">
+                NPSN
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('profile.history') }}"
+                class="block {{ request()->routeIs('profile.history') ? 'text-[#2D336B]' : 'text-[#7886C7]' }} hover:text-blue-900 font-semibold">
+                Sejarah Sekolah
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('profile.teachers') }}"
+                class="block {{ request()->routeIs('profile.teachers') ? 'text-[#2D336B]' : 'text-[#7886C7]' }} hover:text-blue-900 font-semibold">
+                Pendidik dan Tenaga Kependidikan
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('profile.students') }}"
+                class="block {{ request()->routeIs('profile.students') || request()->routeIs('students.show') ? 'text-[#2D336B]' : 'text-[#7886C7]' }} hover:text-blue-900 font-semibold">
+                Peserta Didik
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('profile.extracurricular') }}"
+                class="block {{ request()->routeIs('profile.extracurricular') ? 'text-[#2D336B]' : 'text-[#7886C7]' }} hover:text-blue-900 font-semibold">
+                Ekstrakurikuler
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('profile.dana_bos') }}"
+                class="block {{ request()->routeIs('profile.dana_bos') ? 'text-[#2D336B]' : 'text-[#7886C7]' }} hover:text-blue-900 font-semibold">
+                Bantuan Operasional Sekolah
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('profile.facility') }}"
+                class="block {{ request()->routeIs('profile.facility') ? 'text-[#2D336B]' : 'text-[#7886C7]' }} hover:text-blue-900 font-semibold">
+                Sarana dan Prasarana
+            </a>
+        </li>
     </ul>
 </div>
